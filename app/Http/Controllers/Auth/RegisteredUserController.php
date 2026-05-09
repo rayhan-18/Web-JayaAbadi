83241c9de425e37d2da9be6faf59b68da23688fe
adedwi208 <adeagustus02@gmail.com>
2026-05-09 17:59:31 +0700

    pembaruan fitur register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user);
 
-        return redirect(route('home', absolute: false));
+        return redirect(route('login'));
     }
 }
