63c1e978d816a49bdf5ac657b09e1555d89e27da
adedwi208 <adeagustus02@gmail.com>
2026-06-04 11:40:11 +0700

    bug fixing

--- resources/views/admin/payment/index.blade.php
+++ resources/views/admin/payment/index.blade.php
@@ -373,7 +373,7 @@
 
     <div class="detail-panel" id="paymentPanel">
         <div class="dp-header">
-            <h3>Rincian Pembayaran</h3>
+            <h3>Rincian Pembayaran Payment</h3>
             <div class="dp-close" onclick="closePaymentDetail()"><i class="ti ti-x"></i></div>
         </div>
         <div class="dp-body" id="paymentBody"></div>
